6ac129ae997d68ec2dafcbcbf682eacbae35218d
Diego Alejandro <diego.fc.1@hotmail.com>
2019-04-08 11:34:58 -0500

    RestSErver usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,18 @@
 // ======
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//=====
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//database
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://diegoale1994:<hangar18>@cluster0-cc6wt.mongodb.net/cafe';
+}
+//urlDB = 'mongodb+srv://diegoale1994:hangar18@cluster0-cc6wt.mongodb.net/cafe';
+process.env.URLDB = urlDB;
